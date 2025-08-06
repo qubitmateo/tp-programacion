3e6fb25ea1c4ffe35c8ec9404bfa8d881f587d96
AgusM26 <agustinmiserez010@gmail.com>
2025-08-06 18:41:53 -0300

    Raul

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root = Tk()
 root.title("Bienvenido al programa")
 root.geometry('800x600')
 
-lbl = Label(root, text = "Pantalla principal escrita por agus")
+lbl = Label(root, text = "Pantalla principal escrita por raul")
 lbl.grid()
 
 root.mainloop()
